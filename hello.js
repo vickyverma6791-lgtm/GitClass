421da774d395f7c0f1485f27081fd4dd8eafa24c
vicky <vickyverma6791@gmail.com>
2025-10-07 14:00:20 +0530

    Check

--- hello.js
+++ hello.js
@@ -1,2 +1,8 @@
 console.log("Hellow Gullu Bhaiya"); 
 console.log('modi');
+
+console.log('"check 1');
+console.log('"check 1');
+console.log('"check 1');
+console.log('"check 1');
+console.log('"check 1');
